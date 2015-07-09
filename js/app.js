9a0fbc0a39c95f32fab8105cffacc6317e20db17
jhar <jhar87@gmail.com>
2015-07-08 20:30:51 -0700

    Clicking list items now opens map marker info windows

--- js/app.js
+++ js/app.js
@@ -5,6 +5,9 @@ var mapOptions = {
 };
 var map = new google.maps.Map(document.getElementById('map-canvas'),
     mapOptions);
+var infowindow = new google.maps.InfoWindow({
+			content: "holding..."
+		});
 
 
 function ViewModel() {
@@ -21,27 +24,32 @@ function ViewModel() {
 				new google.maps.Marker({
 					position: new google.maps.LatLng(data.locations[i].lat,data.locations[i].lng),
 					map: map,
-					title: data.locations[i].name
+					title: data.locations[i].name,
 				})
 			);
 		}
 
 		// Add infoWindows to markers
-		var infowindow = new google.maps.InfoWindow({
-			content: "holding..."
-		});
-
 		for (i = 0; i < self.markers().length; i++) {
 			var marker = self.markers()[i];
+			google.maps.event.addListener(marker, 'click', self.openWindow);
+			/*
 			google.maps.event.addListener(marker, 'click', function() {
 				infowindow.setContent(this.title);
 				infowindow.open(map, this);
 			})
+			*/
 		}
 	});
 
-	self.clickedMarker = function() {
-		console.log("clicked");
+	self.openWindow = function() {
+		infowindow.setContent(this.title);
+		infowindow.open(map,this);
+	}
+
+	self.clickedListItem = function(marker) {
+		console.log(marker.title + " " + marker.position);
+		self.openWindow.call(marker);
 	}
 }
 
